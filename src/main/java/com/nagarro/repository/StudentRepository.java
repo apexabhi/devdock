21e790febc9c5aaba142e4d6ff2c147ae1e59585
abhishekkumar24 <abhishek.kumar24@nagarro.com>
2024-04-03 19:17:16 +0530

    devops assignment3

--- src/main/java/com/nagarro/repository/StudentRepository.java
+++ src/main/java/com/nagarro/repository/StudentRepository.java
@@ -1,11 +1,11 @@
 package com.nagarro.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.nagarro.entity.Student;
 
-@Repository
+
 public interface StudentRepository extends JpaRepository<Student,Long> {
 	
 
